5e4fe3b49e8a6bde5f21d7acf8cdfe4f14ec5a51
vishnupreman <vishnupreman1011@gmail.com>
2025-08-15 14:03:10 +0530

    new url added

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: "project-management-git-main-vishnus-projects-ca6d7896.vercel.app", 
+  origin: "time-tracker-app-frontend-a2s87q9aq-vishnus-projects-ca6d7896.vercel.app", 
   credentials: true
 }));
 app.use(express.json());
